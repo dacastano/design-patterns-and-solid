23b9fd1d7b7cc10ee47f87529c290f70121b3a5a
Diego Castano <dacastanoa@unal.edu.co>
2023-07-08 17:37:54 -0500

    Moved store method to a separate class to comply with srp

--- src/main/java/com/company/singleresp/UserController.java
+++ src/main/java/com/company/singleresp/UserController.java
@@ -9,21 +9,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 //Handles incoming JSON requests that work on User
 public class UserController {
 
-    private Store store = new Store();
+    private UserValidator validator = new UserValidator();
+    private UserPersistenceService userPersistenceService = new UserPersistenceService();
     
     //Create a new user
     public String createUser(String userJson) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         User user = mapper.readValue(userJson, User.class);
 
-        UserValidator validator = new UserValidator();
         boolean valid = validator.validateUser(user);
-
         if(!valid) {
             return "ERROR";
         }
 
-        store.store(user);
+        userPersistenceService.saveUser(user);
         
         return "SUCCESS";
     }
